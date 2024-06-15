f2ac2f8c8d56ca9645eaa3b0cea0dc775b1724e1
Matthew Jennings <matt@mattahj.co.uk>
2024-06-15 09:36:43 +0300

    added tests for edit action

--- src/State/MergeBoardReducer.test.ts
+++ src/State/MergeBoardReducer.test.ts
@@ -1,5 +1,5 @@
 import { MergeBoardActionType, MergeBoardInitAction, mergeBoardReducer } from "./MergeBoardReducer";
-import { MergeBoard, Visibility } from "./Types";
+import { Item, MergeBoard, Visibility } from "./Types";
 
 describe("Merge board reducer", () => {
     describe("init action", () => {
@@ -35,5 +35,181 @@ describe("Merge board reducer", () => {
                 mergeBoardReducer(previousState, initAction)
             ).toEqual(newState)
         });
+    });
+
+    describe("edit action", () => {
+
+        let initialBoardState: MergeBoard;
+
+        const getInitialBoardState = () => ({
+            boardId: 'edit_tests_board',
+            height: 3,
+            width: 3,
+            items: [
+                {
+                    itemId: 1173,
+                    itemType: "BroomCabinet_04",
+                    chainId: "BroomCabinet",
+                    pausedUntil: new Date("2024-08-29T12:31:20.783Z"),
+                    createdAt: new Date("2023-12-07T09:48:41.2390000Z"),
+                    visibility: Visibility.Visible,
+                    itemLevel: 4,
+                    isInsideBubble: false
+                },
+                {
+                    itemId: 607,
+                    itemType: "Vase_08",
+                    chainId: "Vase",
+                    pausedUntil: null,
+                    createdAt: new Date("2023-12-12T07:20:35.3010000Z"),
+                    visibility: Visibility.Visible,
+                    itemLevel: 8,
+                    isInsideBubble: false
+                },
+                {
+                    itemId: 1354,
+                    itemType: "PlantedBush_05",
+                    chainId: "PlantedBush",
+                    pausedUntil: null,
+                    createdAt: new Date("2023-12-17T06:56:57.5560000Z"),
+                    visibility: Visibility.Visible,
+                    itemLevel: 5,
+                    isInsideBubble: false
+                },
+                {
+                    itemId: 7080,
+                    itemType: "LevelDownBoosterScissors_01",
+                    chainId: "LevelDownBoosterScissors",
+                    pausedUntil: null,
+                    createdAt: new Date("2023-12-12T18:32:55.9850000Z"),
+                    visibility: Visibility.Visible,
+                    itemLevel: 1,
+                    isInsideBubble: false
+                },
+            ],
+        });
+
+        beforeAll(() => {
+            initialBoardState = getInitialBoardState();
+        });
+
+        it("should allow editing the item type", () => {
+            const desiredState = getInitialBoardState();
+            const desiredItemState = desiredState.items[0] as Item;
+            const desiredType = 'MaintenanceTools_10';
+            desiredItemState.itemType = desiredType;
+
+            expect(
+                mergeBoardReducer(
+                    initialBoardState,
+                    {
+                        type: MergeBoardActionType.EditItem,
+                        itemID: desiredItemState.itemId,
+                        itemType: desiredType
+                    }
+                )
+            ).toStrictEqual(desiredState);
+        });
+
+        it("should allow editing the chain ID", () => {
+            const desiredState = getInitialBoardState();
+            const desiredItemState = desiredState.items[1] as Item;
+            const desiredChainId = 'BroomCabinet';
+            desiredItemState.chainId = desiredChainId;
+
+            expect(
+                mergeBoardReducer(
+                    initialBoardState,
+                    {
+                        type: MergeBoardActionType.EditItem,
+                        itemID: desiredItemState.itemId,
+                        chainId: desiredChainId
+                    }
+                )
+            ).toStrictEqual(desiredState);
+        });
+
+        it("should allow editing the visibility", () => {
+            const desiredState = getInitialBoardState();
+            const desiredItemState = desiredState.items[2] as Item;
+            const desiredVisibility = Visibility.Hidden;
+            desiredItemState.visibility = desiredVisibility;
+
+            expect(
+                mergeBoardReducer(
+                    initialBoardState,
+                    {
+                        type: MergeBoardActionType.EditItem,
+                        itemID: desiredItemState.itemId,
+                        visibility: desiredVisibility
+                    }
+                )
+            ).toStrictEqual(desiredState);
+        });
+
+        it("should allow editing the item level", () => {
+            const desiredState = getInitialBoardState();
+            const desiredItemState = desiredState.items[0] as Item;
+            const desiredLevel = 999999;
+            desiredItemState.itemLevel = desiredLevel;
+
+            expect(
+                mergeBoardReducer(
+                    initialBoardState,
+                    {
+                        type: MergeBoardActionType.EditItem,
+                        itemID: desiredItemState.itemId,
+                        itemLevel: desiredLevel
+                    }
+                )
+            ).toStrictEqual(desiredState);
+        });
+
+        it("should allow editing the bubble state", () => {
+            const desiredState = getInitialBoardState();
+            const desiredItemState = desiredState.items[3] as Item;
+            const desiredBubbleState = true;
+            desiredItemState.isInsideBubble = desiredBubbleState;
+
+            expect(
+                mergeBoardReducer(
+                    initialBoardState,
+                    {
+                        type: MergeBoardActionType.EditItem,
+                        itemID: desiredItemState.itemId,
+                        isInsideBubble: desiredBubbleState
+                    }
+                )
+            ).toStrictEqual(desiredState);
+        });
+
+        it("should allow editing multiple properties at once", () => {
+            const desiredState = getInitialBoardState();
+            const desiredItemState = desiredState.items[3] as Item;
+            desiredItemState.itemType = 'Energy_05';
+            desiredItemState.chainId = 'Energy';
+            desiredItemState.visibility = Visibility.Hidden;
+            desiredItemState.itemLevel = 100000;
+            desiredItemState.isInsideBubble = true;
+
+            expect(
+                mergeBoardReducer(
+                    initialBoardState,
+                    {
+                        type: MergeBoardActionType.EditItem,
+                        itemID: desiredItemState.itemId,
+                        itemType: desiredItemState.itemType,
+                        chainId: desiredItemState.chainId,
+                        visibility: desiredItemState.visibility,
+                        itemLevel: desiredItemState.itemLevel,
+                        isInsideBubble: desiredItemState.isInsideBubble
+                    }
+                )
+            ).toStrictEqual(desiredState);
+        });
+    });
+
+    describe("move action", () => {
+
     });
 });
