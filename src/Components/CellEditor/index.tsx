3165a50aaac759fbf20eeb661c3e64048090584c
Matthew Jennings <matt@mattahj.co.uk>
2024-06-16 15:05:15 +0300

    basic editing for level and chain. stil need to change level to a constrained slider input + derive the min and max levels for each chain

--- src/Components/CellEditor/index.tsx
+++ src/Components/CellEditor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 import Button from "@mui/material/Button";
 
@@ -23,8 +23,27 @@ import {
     Paper,
     Select,
 } from "@mui/material";
-import { Visibility } from "State/Types";
+import { Item, Visibility } from "State/Types";
 
+// Cell Editor is responsible for showing information about a cell in
+// the merge board and letting the user edit it.
+// When editing an item the user can only edit the following fields:
+//
+// - chainID
+// - itemLevel
+// - pausedUntil
+// - visibility
+// - isInsideBubble
+//
+// itemType is not editable because the item type is derived from the
+// combination of chainID and itemLevel.
+//
+// createdAt is not editable because it seems like it should be only able to set when creating items.
+//
+// itemId is not editable because it is automatically generated when creating items.
+// (note: there were duplicates in the data for itemID, so none of the code in
+// this app actually cares about it. Not sure if it's meant to be a UID for
+// each item or points to something external (e.g. entity archetype used to spawn the item?))
 export function CellEditor() {
     const inspectorState = useRequiredContext(MergeBoardInspectorContext);
     const mergeBoardDispatch = useRequiredContext(MergeBoardDispatch);
@@ -75,6 +94,54 @@ export function CellEditor() {
         [mergeBoardDispatch, inspectorState]
     );
 
+    const availableItemChains = useMemo(
+        () => {
+            console.log("deriving item types");
+            return mergeBoardState.items.reduce(
+                (accum: string[], item: Item | null) => {
+                    if (item && !accum.includes(item.chainId)) {
+                        accum.push(item.chainId);
+                    }
+                    return accum;
+                },
+                []
+            );
+        },
+        // Intentionally empty because the items chains should only be derived once at startup.
+        // In a real app I would assume we can query available item chains from some endpoint
+        []
+    );
+
+    const handleItemChainChange = useCallback(
+        (evt: React.ChangeEvent<HTMLInputElement>) => {
+            if (inspectorState.selectedCellIndex !== null) {
+                mergeBoardDispatch({
+                    type: MergeBoardActionType.EditItem,
+                    itemIndex: inspectorState.selectedCellIndex,
+                    chainId: evt.target.value,
+                });
+            }
+        },
+        [mergeBoardDispatch, inspectorState]
+    );
+
+    const handleItemLevelChange = useCallback(
+        (evt: React.ChangeEvent<HTMLInputElement>) => {
+            const val = parseInt(evt.target.value);
+            if (
+                inspectorState.selectedCellIndex !== null &&
+                !Number.isNaN(val)
+            ) {
+                mergeBoardDispatch({
+                    type: MergeBoardActionType.EditItem,
+                    itemIndex: inspectorState.selectedCellIndex,
+                    itemLevel: parseInt(evt.target.value),
+                });
+            }
+        },
+        [mergeBoardDispatch, inspectorState]
+    );
+
     const [showClickTutorial, setShowClickTutorial] = useState(true);
     const onClickTutorialClosed = useCallback(() => {
         setShowClickTutorial(false);
@@ -86,7 +153,7 @@ export function CellEditor() {
     }, [setshowMoveTutorial]);
 
     return (
-        <Box minWidth={450} alignItems={"flex-start"}>
+        <Box minWidth={460} alignItems={"flex-start"}>
             {showClickTutorial && (
                 <Alert
                     severity="info"
@@ -110,31 +177,42 @@ export function CellEditor() {
                 <Paper variant="outlined">
                     <Box padding={2}>
                         {inspectorState.selectedCellIndex !== null && (
-                            <h3 style={{ margin: 0 }}>Edit Item Properties</h3>
+                            <div className="cell-editor-heading">
+                                <div
+                                    className="cell-editor-heading__item"
+                                    style={{
+                                        backgroundImage: `url(public/images/${item.itemType}.webp)`,
+                                    }}
+                                />
+                                <h3 style={{ margin: 0 }}>{item.itemType}</h3>
+                            </div>
                         )}
                         <Divider sx={{ marginY: 1 }} />
                         <FormGroup>
                             <FormControlLabel
-                                label="ID"
+                                label="Item Chain"
                                 className="cell-editor-form-row"
                                 control={
-                                    <Input required value={item?.itemId} />
-                                }
-                            />
-                            <FormControlLabel
-                                label="Type"
-                                className="cell-editor-form-row"
-                                control={
-                                    <Select value={item?.itemType}>
-                                        <MenuItem value={item?.itemType}>
-                                            {item?.itemType}
-                                        </MenuItem>
+                                    <Select
+                                        size="small"
+                                        value={item?.chainId}
+                                        onChange={handleItemChainChange}
+                                    >
+                                        {availableItemChains.map((chainId) => (
+                                            <MenuItem
+                                                value={chainId}
+                                                key={chainId}
+                                            >
+                                                {chainId}
+                                            </MenuItem>
+                                        ))}
                                     </Select>
                                 }
                             />
                             <FormControlLabel
                                 label="Level"
                                 className="cell-editor-form-row"
+                                onChange={handleItemLevelChange}
                                 control={
                                     <Input required value={item?.itemLevel} />
                                 }
